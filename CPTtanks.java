45cba1c32b7c9325a76a083518b3f37b7878e940
KaleaT22 <96299739+KaleaT22@users.noreply.github.com>
2021-12-17 10:14:06 -0500

    Create CPTtanks.java
    This will be the main program

---
+++ CPTtanks.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+
+public class CPTtanks implements ActionListener, KeyboardListener{
+	//properties
+	
+	
+	//methods
+	
+	//constructor
+	public CPTtanks(){
+		
+	}
+	
+	//main method
+	public static void main(String[] args){
+		new CPTtanks();
+	}
+}
